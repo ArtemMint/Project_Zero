9ef65b46caef4a268d169b74d1457f5b557cefcc
ArtemMint <nemnovus@gmail.com>
2021-01-04 21:28:48 +0200

    changed some code :)

--- register/models.py
+++ register/models.py
@@ -8,12 +8,12 @@ class User(models.Model):
     username = models.CharField(max_length=120, blank=False, unique=True)
     email = models.EmailField(max_length=150, blank=False, unique=True)
     password = models.CharField(max_length=100, blank=False)  
-    picture = models.ImageField(upload_to='users', blank=True)
-    first_name = models.CharField(max_length=50, blank=True)
-    last_name = models.CharField(max_length=50, blank=True)
-    phone_number = models.CharField(max_length=15, blank=True)
-    country = models.CharField(max_length=100, blank=True)
-    address = models.CharField(max_length=100, blank=True)     
+    # picture = models.ImageField(upload_to='users', blank=True)
+    # first_name = models.CharField(max_length=50, blank=True)
+    # last_name = models.CharField(max_length=50, blank=True)
+    # phone_number = models.CharField(max_length=15, blank=True)
+    # country = models.CharField(max_length=100, blank=True)
+    # address = models.CharField(max_length=100, blank=True)     
     
     def __str__(self):
         return self.username
